679130589ccf45d7294a4227cf62aafda5369220
superpuper777 <superfevrik@gmail.com>
2023-11-06 00:11:32 +0300

    refactor: fix some behavior of getting data

--- src/routers/Root.tsx
+++ src/routers/Root.tsx
@@ -7,24 +7,23 @@ import SearchBar from '../components/SearchBar';
 import Loading from '../components/Loading';
 import CrashButton from '../components/CrashButton';
 import SearchContext from '../context';
-import { fetchPeople, fetchPeopleBySearchTerm } from '../api';
-import { ResponseData } from '../api/dto';
+import { fetchPeopleBySearchTerm } from '../api';
+import { getStorageByKey } from '../utils/storage';
 
 const Root = (): JSX.Element => {
   const context = useContext(SearchContext);
-  const { setPeople, isError, isLoading, setIsLoading, searchTerm } = context;
+  const { setPeople, isError, isLoading, setIsLoading } = context;
 
   useEffect(() => {
-    const fetchingActions = (data: ResponseData) => {
+    setIsLoading(true);
+
+    fetchPeopleBySearchTerm(
+      '' || (getStorageByKey('searchTerm') as string)
+    ).then((data) => {
       setPeople(data.results);
       setIsLoading(false);
-    };
-    setIsLoading(true);
-    if (searchTerm === '') {
-      fetchPeople().then((data) => fetchingActions(data));
-    } else
-      fetchPeopleBySearchTerm(searchTerm).then((data) => fetchingActions(data));
-  }, [searchTerm, setIsLoading, setPeople]);
+    });
+  }, [setIsLoading, setPeople]);
 
   if (isError) {
     throw new Error('I crashed!');
