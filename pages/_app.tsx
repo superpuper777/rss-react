d5f6ae91d65d4a11f085b750e070fb29bfcdb957
superpuper777 <superfevrik@gmail.com>
2023-11-25 22:40:00 +0300

    refactor: change the Layout, and add ErrorBoundary to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,22 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
 import store from '../store/store';
+import Layout from '../components/Layout';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
+      </ErrorBoundary>
+    </>
   );
 }
 
